79adf6cb122e1c1cffe5575e9ef56d7fecfb6553
Dhairya Patel <p5d7@proton.me>
2023-03-26 16:46:21 +0530

    Update weapon.py
    Added bare bone python code for weapon included in game

---
+++ models/weapon.py
@@ -0,0 +1,21 @@
+class Weapon:
+    def __init__(self, name: str, description: str):
+        self._name = name
+        self._description = description
+        self._is_murder_weapon = False
+
+    # Getter method to retrieve the name of the weapon
+    def get_name(self) -> str:
+        return self._name
+
+    # Getter method to retrieve the description of the weapon
+    def get_description(self) -> str:
+        return self._description
+
+    # Method to set the weapon as the murder weapon
+    def set_murder_weapon(self):
+        self._is_murder_weapon = True
+
+    # Method to check if the weapon is the murder weapon
+    def is_murder_weapon(self) -> bool:
+        return self._is_murder_weapon
